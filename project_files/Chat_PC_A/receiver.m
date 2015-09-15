f118d78a0f43b4fd6f715a261eff1516c01dba0b
Markos Horro <marcos.horro@udc.es>
2015-09-15 16:26:41 +0200

    PROJECT FILES: adding the basic files for the project

---
+++ project_files/Chat_PC_A/receiver.m
@@ -0,0 +1,6 @@
+function [pack, psd, const, eyed] = receiver(tout,fc)
+% Complete the function and remove the following lines
+disp('Complete the receiver') 
+pack = []; psd = [];  const=[]; eyed = [];
+
+end
---
+++ project_files/Chat_PC_B/transmitter.m
@@ -0,0 +1,5 @@
+function transmitter(packet,fc)
+% Complete the function and remove the following lines
+disp('Complete the transmitter')
+
+end
