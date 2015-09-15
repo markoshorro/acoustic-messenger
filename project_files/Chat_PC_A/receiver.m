fba1847b010949890b22490d86c4d6e8fb045abf
Oscar <oscar.peterson91@gmail.com>
2015-09-15 16:32:03 +0200

    RECEIVER: good identation

--- project_files/Chat_PC_A/receiver.m
+++ project_files/Chat_PC_A/receiver.m
@@ -1,6 +1,6 @@
 function [pack, psd, const, eyed] = receiver(tout,fc)
-% Complete the function and remove the following lines
-disp('Complete the receiver') 
-pack = []; psd = [];  const=[]; eyed = [];
+	% Complete the function and remove the following lines
+	disp('Complete the receiver') 
+	xpack = []; psd = [];  const=[]; eyed = [];
 
 end
