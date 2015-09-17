31ca2712a6be0aaaecaaaa359cd7b323c0217714
edwawrdus <oskar.edwertz@gmail.com>
2015-09-17 16:06:38 +0200

    tx: updated tx code and rx B

--- project_files/Chat_PC_A/transmitter.m
+++ project_files/Chat_PC_A/transmitter.m
@@ -1,13 +1,16 @@
-function transmitter(packet,fc)
+%function transmitter(packet,fc)
+packet = randsrc(1,N,[0 1]);
+fc=8000
 
 f_samp = 44*10^3; %sampling freq
 T_samp= 1/f_samp;
 R_b = 444; %bit rate
 
+disp('New test')
 
 N = 432; % number of bits to transmit
 
-m= 2; %QSPK, 4 different options
+m= 2; %QPSK, 4 different options
 M=4; %Alphabet
 
 R_s = R_b/m; %symbol rate
@@ -17,26 +20,40 @@ Tau=1/R_s;
 
 
 
-QPSK_const = [1+1i; -1+1i; -1-1i; 1-1i];
+QPSK_const = [1+1i; -1+1i; 1-1i; -1-1i]/sqrt(2);
 
 
-bits_group = buffer(packet,m)'; % dela upp 2 och 2 (reshape funkar också)
+bits_group = buffer(packet,m)'; % split 2 and 2 (the function reshape also works)
 
-messages = bi2de(bits_group)+1; %kalla varje kombination en siffra
+messages = bi2de(bits_group)+1; %call each combination a number
 
-symbols = QPSK_const(messages) %anknyt varje komb med vår konstellation
+symbols = QPSK_const(messages); %match each number with our constellation
 
 
-scatterplot(symbols);
+%scatterplot(symbols);
 
-symbols_up = upsample(symbols,f_srs); 
+symbols_up = upsample(symbols,round(f_srs)); 
 
 [y,t]=rtrcpuls(0.3,Tau,f_samp,3);
 
 
 
 A=conv(y,symbols_up); 
-                                        
+                    
+figure; subplot(2,1,1); plot(real(pulse_tr_RC), 'b');                         
+                        title('real')
+        subplot(2,1,2); plot(imag(pulse_tr_RC), 'b');                        
+                        title('imag')
+                        
+                        
+
+%freq(A)
+
+A=A.*(cos(2*pi*fc)+sin(2*pi*fc))
+sound(real(A),f_samp)
 
 
-end
+
+
+
+%end
--- project_files/Chat_PC_B/receiver.m
+++ project_files/Chat_PC_B/receiver.m
@@ -1,6 +1,6 @@
 function [pack, psd, const, eyed] = receiver(tout,fc)
 % Complete the function and remove the following lines
-disp('Complete the receiver') 
+disp('Complete the receiver B') 
 pack = []; psd = [];  const=[]; eyed = [];
 
 end
