66190c19708f7a1df880969a43bbad782b25d749
Markos Horro <marcos.horro@udc.es>
2015-09-17 16:08:43 +0200

    Rx: just a template for the function

--- project_files/Chat_PC_A/receiver.m
+++ project_files/Chat_PC_A/receiver.m
@@ -1,6 +1,46 @@
-function [pack, psd, const, eyed] = receiver(tout,fc)
-	% Complete the function and remove the following lines
-	disp('Complete the receiver') 
-	xpack = []; psd = [];  const=[]; eyed = [];
+function [Xhat, psd, const, eyed] = receiver(tout,fc)
+	%% RECEIVER FUNCTION
+    % Group 13
+    % Introduction to Communication Engineering. September 2015 
+    %
+    % Implementation of the receiver.
+    % INPUT:  tout  - type double and defines the time during while
+    %                 waiting
+    %         fc    - carrier frequency
+    %
+    % OUTPUT: Xhat  - Xhat is a row-vector of 432 bits (0, 1) of the
+    %                 received information.
+    %         psd   - psd is a structure with two fields: psd.p is a row
+    %                 vector of the PSD of the received signal calculated
+    %                 after down-conversion in dB. The vector psd.p is normalized
+    %                 that normalized so that maximum value is 0 dB. psd.f is a
+    %                 row-vector of corresponding frequencies in Hz
+    %         const - row-vector of complex samples after downsampling that 
+    %                 correspond to information par
+    %         eyed  - eyed.fsfd is an integer and stands for a number of 
+    %                 samples per symbol; eyed.r is a complex row-vector of
+    %                 information part of the received signal after matched
+    %                 filtering and timing synchronization (properly cut so
+    %                 that MATLAB eyediagram(eyed.r, eyed.fsfd) plots a 
+    %                 desired eye diagram).
+    %%
+	%% Some parameters
+    f_samp = 44*10^3; %sampling freq
+    T_samp= 1/f_samp;
+    R_b = 444; %bit rate
+    N = 432; % number of bits to transmit
+    m= 2; %QSPK, 4 different options
+    M=4; %Alphabet    
+    
+    %% Passband to baseband
+    
+    %% Syncronization
+    
+    %% Demodulation
+    QPSK_const = [1+1i; -1+1i; -1-1i; 1-1i];
 
+    
+    %% Symbol to bits
+    % we should have a 2D vector
+    
 end
