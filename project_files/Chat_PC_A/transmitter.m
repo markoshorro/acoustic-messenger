1ce9a67894c56caebc029d6b58b3b3fabbfabc9e
edwawrdus <oskar.edwertz@gmail.com>
2015-09-22 16:11:00 +0200

    Tx: working carrier freq

--- project_files/Chat_PC_A/transmitter.m
+++ project_files/Chat_PC_A/transmitter.m
@@ -2,8 +2,8 @@
 
 fc=800
 
-f_samp = 44*10^3; %sampling freq
-T_samp= 1/f_samp;
+fs = 44*10^3; %sampling freq
+T_samp= 1/fs;
 R_b = 444; %bit rate
 
 disp('New test')
@@ -16,7 +16,7 @@ m= 2; %QPSK, 4 different options
 M=4; %Alphabet
 
 R_s = R_b/m; %symbol rate
-f_srs = f_samp/R_s; %number of samples in each symbol
+f_srs = fs/R_s; %number of samples in each symbol
 
 Tau=1/R_s;
 
@@ -36,7 +36,7 @@ symbols = QPSK_const(messages); %match each number with our constellation
 
 symbols_up = upsample(symbols,round(f_srs)); 
 
-[y,t]=rtrcpuls(0.3,Tau,f_samp,3);
+[y,t]=rtrcpuls(0.3,Tau,fs,3);
 
 
 
@@ -48,10 +48,15 @@ A=conv(y,symbols_up);
 %                         title('imag')
                         
                         
-size(A)
+S=size(A)
 
-                        
-%A= A*exp(1i.*2.*pi.*t);
+t=0:1/fs:(Tau*S(1)-Tau);
+
+Q=A.*cos(1i*2*pi*t)
+size(Q)
+
+
+A= A.*exp(1i*2*pi*t*fc);
 
 A=real(A);
 
@@ -61,7 +66,7 @@ N=length((real(A))); %length of one vector, i.e our window length (time)
 %N=pow2(nextpow2(L)); %length of our fft (total bins)
 N=1024;
 
-f_vec=(f_samp/N)*(-floor(N/2):1:ceil(N/2)-1);
+f_vec=(fs/N)*(-floor(N/2):1:ceil(N/2)-1);
 
 % figure(89)
 % plot(f_vec,fft(A,N))
@@ -70,7 +75,7 @@ f_vec=(f_samp/N)*(-floor(N/2):1:ceil(N/2)-1);
 figure(50)
 plot(f_vec,20*log10((abs(fftshift(fft(A,N)))).^2))
 
-sound(real(A),f_samp)
+sound(real(A),fs)
 
 
 
--- project_files/Chat_PC_B/transmitter.m
+++ project_files/Chat_PC_B/transmitter.m
@@ -1,4 +1,8 @@
 %load '../parameters.m'
+span=6;
+R_b=440;
+R_s=R_b/m;
+f_srs=fs/R_s;
 packet = randsrc(1,N,[0 1]);        % Just for test
 
 bits_group = buffer(packet,m)';     % split 2 and 2 (the function reshape also works)
@@ -9,19 +13,22 @@ symbols_up = upsample(symbols,round(f_srs)); % Space the symbols fsfd apart, to 
 [si,t] = rtrcpuls(0.3, Tau, f_samp, span);
 s=conv(si,symbols_up); 
                     
-figure; subplot(2,1,1); plot(real(s), 'b');                         
-                        title('real')
-        subplot(2,1,2); plot(imag(s), 'b');                        
-                        title('imag')
+% figure; subplot(2,1,1); plot(real(s), 'b');                         
+%                         title('real')
+%         subplot(2,1,2); plot(imag(s), 'b');                        
+%                         title('imag')
 s_tailness = s(f_srs*span:end-f_srs*span);
 
-figure; subplot(2,1,1); plot(real(s_tailness), 'b');                         
-                        title('real')
-        subplot(2,1,2); plot(imag(s_tailness), 'b');                        
-                        title('imag')
+% figure; subplot(2,1,1); plot(real(s_tailness), 'b');                         
+%                         title('real')
+%         subplot(2,1,2); plot(imag(s_tailness), 'b');                        
+%                         title('imag')
 
-                        
+ t=(0:1/fs:((length(symbols)/216)-1/fs)).';
+ t = t(1:length(s_tailness));
+
+ fc= 5000;                       
 t_tx = 1:length(s_tailness);
-s_passband = s_tailness.*(t_tx');
+s_passband = real(s_tailness.*(sqrt(2)*exp(1i*2*pi*fc*t)));
 figure;
-pwelch(s_passband,hamming(512),[],[],T_samp,'centered');
+pwelch(s_passband,hamming(512),[],[],f_samp,'centered');
