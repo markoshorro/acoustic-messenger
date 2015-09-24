4cb83e662ff047108477316ab2cbc0ec42796bcb
edwawrdus <oskar.edwertz@gmail.com>
2015-09-24 14:48:48 +0200

    changed variable names

--- project_files/Chat_PC_A/transmitter.m
+++ project_files/Chat_PC_A/transmitter.m
@@ -18,15 +18,15 @@ function transmitter(packet, fc)
     messages = bi2de(bits_group)+1;     % call each combination a number
     symbols = const(messages);          % match each number with our constellation
 
-    symbols_up = upsample(symbols,round(f_srs)); % Space the symbols fsfd apart, to enable pulse shaping using conv.
-    [si,~] = rtrcpuls(0.3, Tau, f_samp, span);
+    symbols_up = upsample(symbols,round(sps)); % Space the symbols fsfd apart, to enable pulse shaping using conv.
+    [si,~] = rtrcpuls(0.3, Tau, fs, span);
     s=conv(si,symbols_up); 
 
     % figure; subplot(2,1,1); plot(real(s), 'b');                         
     %                         title('real')
     %         subplot(2,1,2); plot(imag(s), 'b');                        
     %                         title('imag')
-    s_tailness = s(f_srs*span:end-f_srs*span);
+    s_tailness = s(sps*span:end-sps*span);
 
     % figure; subplot(2,1,1); plot(real(s_tailness), 'b');                         
     %                         title('real')
@@ -38,6 +38,6 @@ function transmitter(packet, fc)
                       
     s_passband = real(s_tailness.*(sqrt(2)*exp(1i*2*pi*fc*t)));
     figure;
-    pwelch(s_passband,hamming(512),[],[],f_samp,'centered');
+    pwelch(s_passband,hamming(512),[],[],fs,'centered');
 
 end
--- project_files/parameters.m
+++ project_files/parameters.m
@@ -7,10 +7,10 @@ const = [1+1i; -1+1i; -1-1i; 1-1i];
 
 %% Sampling freq
 fs = 44*10^3;
-T_samp = 1/fs;
+Ts = 1/fs;
 
 %% Bit rate
-R_b = 440;
+Rb = 440;
 
 %% Number of bits to transmit
 N = 432;
@@ -22,13 +22,13 @@ m = 2;
 M = 4; 
 
 %% Symbol rate
-R_s = R_b/m;
+Rs = Rb/m;
 
 %% Symbol time
-Tau=1/R_s;
+Tau=1/Rs;
 
 %% Span
 span=6;
 
-%% Samples for symbol
-f_srs=fs/R_s;
+%% Samples per symbol
+sps=fs/Rs;
