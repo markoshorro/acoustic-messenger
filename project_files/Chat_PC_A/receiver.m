5bd81e68159127cd69bb59333cfd611c27efe8b8
Oscar <oscar.peterson91@gmail.com>
2015-10-08 17:26:02 +0200

    Rx: Pilot + barker changes, Tx: minor comments

--- project_files/Chat_PC_A/receiver.m
+++ project_files/Chat_PC_A/receiver.m
@@ -26,21 +26,18 @@
     %
 	%% Some parameters
     run('../parameters.m')
-    
+    %close all;
     % Testing
-    fc = 4000;
+    fc = 6000;
     
     %% Audio data collection
     channels = 1;   
-    recordBits = 16; % WHAT ABOUT THIS? 8 or 16 bits? Should we ask?
+    recordBits = 16;
     
     % Preamble stuff
-    [si,~] = rtrcpuls(rollOff, Tau, fs, span);
-    symbolsBarker = constBPSK(symbBarker);
-    pulseBarker = conv(upsample(symbolsBarker, round(sps)), si);
-    t = ((1:length(pulseBarker))/fs);
-    barkerPass = real(pulseBarker.*(exp(1i*2*pi*fc*t)));
-    barkerPass = barkerPass/max(barkerPass);
+%     [si,~] = rtrcpuls(rollOff, Tau, fs, span);
+%     symbolsBarker = constBPSK(symbBarker);
+%     pulseBarker = conv(upsample(symbolsBarker, round(sps)), si);
 
 %     figure(4); subplot(2,1,1); plot(real(barkerPass), 'b');                         
 %                          title('real')
@@ -73,62 +70,111 @@
 %         end     
 %     end
         
-                
+      %pause(1);      
 %     while message(end) == 0.5;    % marker condition (dummy)
-        while toc < 1;              % waits for 'toc' seconds to record     
-        end                 
+        while toc < 4;              % waits for 'toc' seconds to record     
+        end
+        
+        
+        
 %        pause(recording);          % Pause recording
 %        message = getaudiodata(recording,'single'); %fetch data
 %        resume(recording);                         
 %     end
     message = getaudiodata(recording,'single');
-    stop(recording);                % Stop recording after finding correct packet size
     
-    % TO TRY -> this should get the peak value
-    corr = conv(message, fliplr(barkerPass(sps*span:end-sps*span)));
-    figure(11);
-    plot(corr);
+%     N = max(1024,length(message));
+%     P = fftshift(fft(message,N));     
+%     fvec = (fs/N).*[0:(N-1)];
+%     fvec = (fvec < fs/2); 
+%    % figure(8); plot(fvec,20*log10(abs(P)));  
+%     
+%     xdftYouCareAbout = abs(P(1:round(N/2))); % Take the absolute magnitude.
+% 
+% [maxVal, index] = max(xdftYouCareAbout); % maxVal is your (un-normalized) maximum amplitude
+% 
+% maxFreq = freqsYouCareAbout(index); % This is the frequency of your dominant signal. 
+    
+%     tesdf= 20*log10(abs(P))
+%    
+%     [~,b] = max(fvec);
+%     b
+
+    stop(recording);                % Stop recording after finding correct packet size
     
     %% Passband to baseband
     t = ((1:length(message))/fs).';
     data = message.*(exp(-1i*2*pi*fc*t));
     
+%     pwelch(data, hamming(128),[],[],fs,'twosided');
+    
+    %% Low pass filter
+    order = 2^10;
+    Wn = 2*cutOff/fs;
+    lpf = fir1(order, Wn);    
+    %data = conv(data, lpf);
+    
+%     pwelch(data, hamming(128),[],[],fs,'twosided');
+    
     %% Demodulation (MF)
     yt = conv(si, data);            % Convolution between received signal and rtrc pulse
-    yt = yt(sps*span:end-sps*span);
-    
-%     figure(111);
-%     subplot(2,1,1);
-%     plot(real(yt));
-%     subplot(2,1,2);
-%     plot(imag(yt));
-% 
-%% Debugging symbols correlation to peaks
+    %yt = yt(sps*span:end-sps*span);
 
-h = 1:length(yt)/length(const):length(yt);
-figure(5)
-plot(real(yt),'k'); hold on; plot(h,real(const),'*r')
-
-    %% Decision: correct for QPSK and 8PSK
+    [si,~] = rtrcpuls(rollOff, Tau, fs, span);
+    symbolsBarker = constBPSK(symbBarker);
     const = downsample(yt, sps);        % Downsampling
-    scatterplot(const);                 % Plotting Constellations from received signal
-    constAngle = angle(const);          % Getting angles from received signal constellation
-    indexSymb = zeros(length(const),1); % Declaration before for-loop
+    pulseBarker = conv(symbolsBarker, si);
     
-    for i = 1:length(const)             % @TODO: Somehow remove for-loop for more efficiency?
-        [~,x] = min(abs(constAngle(i)-QPSKAngle)); 
-        indexSymb(i) = x;               % Matching each symbol with correct constellation
-    end;
+    % TO TRY -> t   his should get the peak value
+    corr = conv(const, fliplr(symbolsBarker));
+    figure(122);
+    title('Correlation');
+    plot(abs(corr));
     
-    symbolsRec = indexSymb-1;
-    
-    bitsGroup = de2bi(symbolsRec);
+        [~,PackStart] = max(abs(corr));
+
+        PackStart = PackStart
+        
+    %     figure(111);
+    %     subplot(2,1,1);
+    %     plot(real(yt));
+    %     subplot(2,1,2);
+    %     plot(imag(yt));
+
+        %% Decision: correct for QPSK and 8PSK
+
+        constPack = const(PackStart+10:PackStart+10+Ns-1);
+        constPilot = const(PackStart:PackStart+9);
+        constBarker = const(PackStart-nBarker:PackStart-1);
+
+        scatterplot(constPack);                 % Plotting Constellations from received signal
+        scatterplot(constBarker);
+        scatterplot(constPilot);
+        constAngle = angle(constPack);          % Getting angles from received signal constellation
+        indexSymb = zeros(length(constPack),1); % Declaration before for-loop
+
+        for i = 1:length(constPack)             % @TODO: Somehow remove for-loop for more efficiency?
+            [~,x] = min(abs(constAngle(i)-QPSKAngle)); 
+            indexSymb(i) = x;               % Matching each symbol with correct constellation
+        end;
+
+        symbolsRec = indexSymb-1;
+
+        bitsGroup = de2bi(symbolsRec);
+
+
+        h = 1:length(yt)/length(constPack):length(yt);
+%     figure(123)
+%     plot(real(yt),'k')
+%     hold on
+%     plot(h,real(constPack),'*r')
+%     hold off
     
     %% XHAT output
     Xhat = reshape(bitsGroup.',[1,m*length(bitsGroup)]);
     
     %% PSD output
-    XhatdB = 20*log10(const);
+    XhatdB = 20*log10(constPack);
     [psdXhat, fXhat] = pwelch(abs(XhatdB),...
                                 hamming(128),[],[],fs,'twosided');
     psd = struct('p',psdXhat,'f',fXhat);
--- project_files/Chat_PC_B/transmitter.m
+++ project_files/Chat_PC_B/transmitter.m
@@ -1,4 +1,4 @@
-function transmitter(packet, fc)
+%function transmitter( packet, fc )
 	%% TRANSMITTER FUNCTION
     % Group 13
     % Introduction to Communication Engineering. September 2015 
@@ -7,43 +7,67 @@ function transmitter(packet, fc)
     % INPUT:  packet  - vector with data
     %         fc    - carrier frequency
     %
-    % OUTPUT: SOUND to the speaker (
+    % OUTPUT: SOUND to the speaker
     %
     
     run('../parameters.m');
+    close all;
+    pilot = ones(1,10);
+    packet = [pilot randsrc(1,N,[1 1])];        % Just for test
+    fc = 4000;
+
+    % Split in m columns
+    bitsGroup = buffer(packet,m)';     
+    messages = bi2de(bitsGroup)+1;
     
-    packet = randsrc(1,N,[0 1]);        % Just for test
-    fc=5000;
-
-    bits_group = buffer(packet,m)';     % split 2 and 2 (the function reshape also works)
-    messages = bi2de(bits_group)+1;     % call each combination a number
-    symbols = constQPSK(messages);          % match each number with our constellation
-
-    symbols_up = upsample(symbols,round(sps)); % Space the symbols fsfd apart, to enable pulse shaping using conv.
-    [si,~] = rtrcpuls(0.3, Tau, fs, span);
-    s=conv(si,symbols_up); 
+    % Match each number with our constellation
+    symbols = constQPSK(messages);
     
-%     figure; subplot(2,1,1); plot(real(s), 'b');                         
-%                              title('real')
-%              subplot(2,1,2); plot(imag(s), 'b');                        
-%                              title('imag')
-    s_tailness = s(sps*span:end-sps*span);
+    % Match barker code with BPSK constellation
+    symbolsBarker = constBPSK(symbBarker);
+    symbols = [symbolsBarker.'; symbols];
+    
+    % Space the symbols fsfd apart, to enable pulse shaping using conv
+    symbolsUp = upsample(symbols, round(sps));
+        
+    % Pulse convolution
+    [si,~] = rtrcpuls(rollOff, Tau, fs, span);
+    s = conv(si, symbolsUp); 
+    
+    % Getting convoluted signal without heads nor tails
+    sTailless = s(sps*span:end-sps*span);
+    
+    % Converting onto passband signal
+    t = ((1:length(sTailless))/fs).';
+    sPassband = real(sTailless.*(exp(1i*2*pi*fc*t)));
 
-%     figure; subplot(2,1,1); plot(real(s_tailness), 'b');                         
-%                              title('real')
-%              subplot(2,1,2); plot(imag(s_tailness), 'b');                        
-%                              title('imag')
+    % Normalized values
+    sPassband = sPassband/max(sPassband);
+    
+    % Output through the speaker
+    sound(sPassband, fs);
 
-    t = (0:1/fs:1-1/fs).';
-    t = t(1:length(s_tailness));
-                      
-    s_passband = real(s_tailness.*(sqrt(2)*exp(1i*2*pi*fc*t)));
+    %% DEBUGGING
+    figure(1); subplot(2,1,1); plot(real(sTailless), 'b');                         
+                             title('real')
+             subplot(2,1,2); plot(imag(sTailless), 'r');                        
+                             title('imag')
+    figure(2); subplot(2,1,1); plot(real(s), 'b');                         
+                             title('real')
+             subplot(2,1,2); plot(imag(s), 'r');                        
+                             title('imag')
+
+    figure(3); subplot(2,1,1); plot(real(sPassband), 'b');                         
+                             title('real')
+             subplot(2,1,2); plot(imag(sPassband), 'r');                        
+                             title('imag')
 %     figure(1);
-%     pwelch(s_passband,hamming(512),[],[],fs,'centered');
+%     pwelch(sPassband,hamming(512),[],[],fs,'centered');
 %     
 %     figure(2)
-%     plot(s_passband)
-    s_passband = s_passband/max(s_passband);
-    sound(s_passband,44e3);
-  
-end
+%     plot(sPassband)
+%     figure; subplot(2,1,1); plot(real(sTailness), 'b');                         
+%                              title('real')
+%              subplot(2,1,2); plot(imag(sTailness), 'b');                        
+%                              title('imag')
+%end
